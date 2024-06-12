eadca34d87a0360f25b91e5c12a669b1f72b43b5
JurajSupolik <jurajsupolik@gmail.com>
2024-06-12 22:21:17 +0200

    FINAL JURAJ

---
+++ scripts/word-list.js
@@ -0,0 +1,155 @@
+const wordList = [
+    {
+        word: "gitara",
+        hint: "Hudobný nástroj so strunami."
+    },
+
+    {
+        word: "hora",
+        hint: "Veľké prirodzené vyvýšenie povrchu Zeme."
+    },
+
+    {
+        word: "futbal",
+        hint: "Populárny šport hraný s guľatou loptou."
+    },
+
+
+
+    {
+        word: "pizza",
+        hint: "Slané jedlo pozostávajúce z okrúhlej, sploštenej základne s polevami."
+    },
+    {
+        word: "jazz",
+        hint: "Žáner hudby charakterizovaný improvizáciou a synkopáciou."
+    },
+    {
+        word: "kamera",
+        hint: "Zariadenie používané na zachytávanie a nahrávanie obrazov alebo videí."
+    },
+    {
+        word: "diamant",
+        hint: "Vzácny drahokam známy svojou brilanciou a tvrdosťou."
+    },
+
+    {
+        word: "veda",
+        hint: "Systematické štúdium štruktúry a správania fyzického a prírodného sveta."
+    },
+    {
+        word: "bicykel",
+        hint: "Človekom poháňané vozidlo s dvoma kolesami."
+    },
+
+    {
+        word: "tanec",
+        hint: "Rytmický pohyb tela často vykonávaný na hudbu."
+    },
+    {
+        word: "galaxia",
+        hint: "Rozsiahly systém hviezd, plynu a prachu držaný pohromade gravitáciou."
+    },
+    {
+        word: "orchester",
+        hint: "Veľký súbor hudobníkov hrajúcich na rôzne nástroje."
+    },
+    {
+        word: "sopka",
+        hint: "Hora alebo kopec s otvorom, cez ktorý je vyvrhovaná láva, úlomky skál, horúca para a plyn."
+    },
+
+    {
+        word: "socha",
+        hint: "Trojrozmerná umelecká forma vytvorená tvarovaním alebo spájaním materiálov."
+    },
+
+    {
+        word: "balet",
+        hint: "Klasická tanečná forma charakterizovaná presnými a pôvabnými pohybmi."
+    },
+    {
+        word: "astronaut",
+        hint: "Osoba vyškolená na cestovanie a prácu vo vesmíre."
+    },
+
+
+
+
+    {
+        word: "dovolenka",
+        hint: "Obdobie venované zábave, odpočinku alebo relaxácii."
+    },
+
+    {
+        word: "divadlo",
+        hint: "Budova alebo vonkajší priestor, kde sa konajú predstavenia, filmy alebo iné podujatia."
+    },
+    {
+        word: "mobil",
+        hint: "Zariadenie používané na komunikáciu,hranie hier,fotenie...."
+    },
+    {
+        word: "jazyk",
+        hint: "Systém komunikácie pozostávajúci zo slov, gest a syntaxe."
+    },
+
+
+    {
+        word: "teleskop",
+        hint: "Optický prístroj používaný na pozorovanie vzdialených objektov vo vesmíre."
+    },
+
+    {
+        word: "fotografia",
+        hint: "Umenie, proces alebo prax vytvárania obrazov zaznamenávaním svetla alebo iného elektromagnetického žiarenia."
+    },
+    {
+        word: "safari",
+        hint: "Expedícia alebo cesta, zvyčajne za účelom pozorovania divokej zveri v ich prirodzenom prostredí."
+    },
+
+    {
+        word: "rieka",
+        hint: "Veľký prírodný prúd vody tečúci v kanáli do mora, jazera alebo iného takého prúdu."
+    },
+
+
+    {
+        word: "paradox",
+        hint: "Výrok alebo situácia, ktorá si protirečí alebo odporuje intuícii."
+    },
+    {
+        word: "hlavolam",
+        hint: "Hra, hračka alebo problém určený na testovanie dôvtipu alebo vedomostí."
+    },
+
+    {
+        word: "tajomstvo",
+        hint: "Niečo, čo je skryté alebo neznáme iným."
+    },
+
+
+
+
+
+    {
+        word: "nostalgia",
+        hint: "Sentimentálna túžba alebo túžobná náklonnosť k minulosti."
+    },
+
+    {
+        word: "auto",
+        hint: "dopravný prostriedok ktorý má 4 kolesá"
+    },
+    {
+        word: "nechet",
+        hint: "Niečo čo máme na prste."
+    },
+    {
+        word: "lego",
+        hint: "Svetoznáma stavebnica pre deti."
+        },
+       
+
+];
